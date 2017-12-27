8540df6539215181a34de05372ca27dd85a60561
Ken Bannister <kb2ma@runbox.com>
2017-12-27 08:08:33 -0500

    Add web home page, including Bootstrap and navbar.

--- ratnet/main.py
+++ ratnet/main.py
@@ -11,10 +11,12 @@ Entry point for Ratnet application.
 Usage:
    $. ./main.py
 '''
-import sys
-import logging
+import sys, logging
 
 from flask import Flask, render_template, redirect, request, abort
+from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View, Subgroup, Link, Text, Separator
 
 log = logging.getLogger(__name__)
 
@@ -23,7 +25,42 @@ log = logging.getLogger(__name__)
 #
 
 app = Flask('ratnet')
+Bootstrap(app)
 
+# Defaults for application settings
+
+# Used to generate URI prefix below.
+app.config['APPLICATION_ROOT'] = '/'
+
+# Allow override config settings from a file
+try:
+    app.config.from_envvar('RATNET_CONFIG_FILE')
+except:
+    log.exception('Can\'t read config file; using defaults')
+
+# Prefix for URI path for web server. Used for app route decorators.
+URI_PREFIX = app.config['APPLICATION_ROOT']
+if URI_PREFIX == None or URI_PREFIX == '/':
+    URI_PREFIX = ''
+
+# Navbar definitions
+nav = Nav()
+nav.register_element('frontend_top', Navbar(
+    View('Ratnet', '.index'),
+))
+nav.init_app(app)
+
+#
+# Routes
+#
+
+@app.route('{0}/'.format(URI_PREFIX))
+def index():
+    return render_template('index.html')
+
+#
+# Main startup
+#
 
 # Run from command line. Runs web server in threaded mode. Although the
 # web server is not designed for production, this allows us to handle
